dc86bc96a6c0483b6540b4db3dd52cc40d9232d7
Saikrishna <team@greek.sh>
2025-02-14 10:15:11 +0530

    loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { LoaderComponent } from './components/loader/loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent,LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
