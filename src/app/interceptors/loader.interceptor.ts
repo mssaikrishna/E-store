83f699549f8e035521f8cac3a5c839d3f811c392
Saikrishna <team@greek.sh>
2025-02-14 10:08:30 +0530

    nil

--- src/app/interceptors/loader.interceptor.ts
+++ src/app/interceptors/loader.interceptor.ts
@@ -6,13 +6,11 @@ import { finalize } from 'rxjs/operators';
 export const loaderInterceptor: HttpInterceptorFn = (req, next) => {
   const loaderService = inject(LoaderService);
   console.log('Loader Interceptor Triggered');
-  debugger;
   loaderService.show();
 
   return next(req).pipe(
     finalize(() => {
       console.log('Loader Interceptor Finalized');
-      debugger; // Stop execution here
       loaderService.hide();
     })
   );
