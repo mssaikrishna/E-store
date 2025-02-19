82c0efe64c7691074a5559999e5789473891e09e
Saikrishna <saik903@gmail.com>
2025-02-19 16:28:25 +0530

    custom directive

---
+++ src/app/shared/directive/text-color.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appTextColor]'
+})
+export class TextColorDirective implements OnInit {
+  @Input() appTextColor = 'blue'; // Default color
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit() {
+    this.el.nativeElement.style.color = this.appTextColor;
+  }
+}
